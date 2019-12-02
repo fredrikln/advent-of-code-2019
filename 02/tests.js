12a8973637557a43fc9a07da5b6a8c9cfc5869bb
Fredrik Larsson <fredrikln@gmail.com>
2019-12-02 11:23:29 +0100

    Fix tests, prepare Intcode computer for future changes

--- 02/tests.js
+++ 02/tests.js
@@ -1,36 +1,36 @@
 let { assert } = require('chai');
 
-const part1 = require('./part1');
+const { intcode, parseMemoryFromString } = require('../utils/intcode');
 
 describe('Day 2', function () {
   describe('Part 1', function () {
     it('1,0,0,0,99', function () {
-      let input = '1,0,0,0,99';
-      let output = part1(input);
+      let input = parseMemoryFromString('1,0,0,0,99');
+      let output = intcode(input)();
 
       assert.equal(output.toString(), '2,0,0,0,99');
       assert.equal(output[0], 2);
     });
 
     it('2,3,0,3,99', function () {
-      let input = '2,3,0,3,99';
-      let output = part1(input);
+      let input = parseMemoryFromString('2,3,0,3,99');
+      let output = intcode(input)();
 
       assert.equal(output.toString(), '2,3,0,6,99');
       assert.equal(output[0], 2);
     });
 
     it('2,4,4,5,99,0', function () {
-      let input = '2,4,4,5,99,0';
-      let output = part1(input);
+      let input = parseMemoryFromString('2,4,4,5,99,0');
+      let output = intcode(input)();
 
       assert.equal(output.toString(), '2,4,4,5,99,9801');
       assert.equal(output[0], 2);
     });
 
     it('1,1,1,4,99,5,6,0,99', function () {
-      let input = '1,1,1,4,99,5,6,0,99';
-      let output = part1(input);
+      let input = parseMemoryFromString('1,1,1,4,99,5,6,0,99');
+      let output = intcode(input)();
 
       assert.equal(output.toString(), '30,1,1,4,2,5,6,0,99');
       assert.equal(output[0], 30);
--- utils/intcode.js
+++ utils/intcode.js
@@ -1,26 +1,40 @@
-module.exports.intcode = (memory) => (noun = 12, verb = 2) => {
+const ADD = 1;
+const MULTIPLY = 2;
+const HALT = 99;
+
+module.exports.intcode = (memory) => (noun = null, verb = null) => {
   // Create a copy of the memory as the current program
   const program = memory.slice(0)
 
-  program[1] = noun;
-  program[2] = verb;
+  // Load noun and verb if available (not available in Day 2 examples for test)
+  if (noun !== null) program[1] = noun;
+  if (verb !== null) program[2] = verb;
 
-  for (let i = 0; i < program.length; i += 4) {
-    const a = program[i+1];
-    const b = program[i+2];
-    const output = program[i+3];
+  // Start pointer at 0
+  let pointer = 0;
 
-    switch(program[i]) {
-      case 1:
+  while (pointer < program.length) {
+    const instruction = program[pointer];
+    const a = program[pointer+1];
+    const b = program[pointer+2];
+    const output = program[pointer+3];
+
+    switch(instruction) {
+      case ADD:
         program[output] = program[a] + program[b];
+        pointer += 4;
         break;
-      case 2:
+
+      case MULTIPLY:
         program[output] = program[a] * program[b];
+        pointer += 4;
         break;
-      case 99:
+
+      case HALT:
         return program;
+
       default:
-        throw new Error(`Unknown opcode at position ${i}: ${program[i]}`);
+        throw new Error(`Unknown opcode at position ${pointer}: ${instruction}`);
         break;
     }
   }
