59b3a7a7ec6fa37d6b0fe988b9966682e9548552
Fredrik Larsson <fredrikln@gmail.com>
2019-12-03 16:54:05 +0100

    Add a template folder to copy for each new day

---
+++ template/tests.js
@@ -0,0 +1,18 @@
+let { assert } = require('chai');
+
+const part1 = require('./part1');
+const part2 = require('./part2');
+
+describe('Day X', function () {
+  describe('Part 1', function () {
+    it('Test case 1', function () {
+      assert.equal(true, true);
+    });
+  });
+
+  describe('Part 2', function () {
+    it('Test case 1', function () {
+      assert.equal(true, true);
+    });
+  });
+});
